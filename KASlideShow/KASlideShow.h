fbc0566e1263330e0f6a0cce9a91c65f8ef306ff
Alexis Creuzot <alexis@monoqle.fr>
2015-03-16 11:10:01 +0100

    add import

--- KASlideShow/KASlideShow.h
+++ KASlideShow/KASlideShow.h
@@ -15,6 +15,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#import <UIKit/UIKit.h>
+
 typedef NS_ENUM(NSInteger, KASlideShowTransitionType) {
     KASlideShowTransitionFade,
     KASlideShowTransitionSlide
